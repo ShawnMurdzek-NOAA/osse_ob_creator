1aa961edacff051c9a4c6d539db6927a9e386642
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-03-04 20:57:10 -0600

    Add superob ob creation and plotting to create_syn_ob_jobs.py

--- plotting/plot_raw_superob_uas_vprofs.py
+++ plotting/plot_raw_superob_uas_vprofs.py
@@ -1,6 +1,11 @@
 """
 Plot Raw and Superobbed UAS Vertical Profiles
 
+Optional command-line arguments:
+    argv[1] = Prepbufr file tag
+    argv[2] = Time of prepbufr file (YYYYMMDDHH)
+    argv[3] = YAML file with program parameters
+
 shawn.s.murdzek@noaa.gov
 Date Created: 29 February 2024
 """
@@ -32,7 +37,7 @@ bufr_file_raw = '/work2/noaa/wrfruc/murdzek/nature_run_spring/obs/uas_hspace_150
 # BUFR file with superobbed UAS profile
 bufr_file_superob = '/work2/noaa/wrfruc/murdzek/src/osse_ob_creator/main/tmp_superob.csv'
 
-# Ob type for thermodynamic and wind meaurements 
+# Ob type for thermodynamic and wind measurements 
 ob_typ_thermo = 136
 ob_typ_wind = 236
 
@@ -41,6 +46,21 @@ all_sid = ["'UA000001'", "'UA000053'", "'UA000180'"]
 
 # Output file name (include %s placeholder for SID)
 out_fname = './uas_raw_superob_compare_%s.png'
+
+# Use passed arguments, if they exist
+if len(sys.argv) > 1:
+    tag = sys.argv[1]
+    t_str = sys.argv[2]
+    with open(sys.argv[3], 'r') as fptr:
+        param = yaml.safe_load(fptr)
+    bufr_file_raw = '%s/%s.%s.input.prepbufr.csv' % (param['paths']['syn_superob_csv'],
+                                                     t_str, tag)
+    bufr_file_superob = '%s/%s.%s.fake.prepbufr.csv' % (param['paths']['syn_superob_csv'],
+                                                        t_str, tag)
+    ob_type_thermo = int(param['superobs']['plot_vprof']['ob_type_thermo'])
+    ob_type_wind = int(param['superobs']['plot_vprof']['ob_type_wind'])
+    all_sid = param['superobs']['plot_vprof']['all_sid']
+    out_fname = '%s/%s' % (param['paths']['plots'], t_str) +'/uas_raw_superob_compare_%s.png'
 
 
 #---------------------------------------------------------------------------------------------------
