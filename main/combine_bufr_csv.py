11ca87fcd65d5362ea79aa97f6b92bbe05e1feb7
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-03-06 14:55:20 -0600

    Change combine CSV step to combine an arbitrary number of CSV files

--- main/combine_bufr_csv.py
+++ main/combine_bufr_csv.py
@@ -1,10 +1,9 @@
 """
-Combine Two BUFR CSV Files
+Combine An Arbitrary Number of BUFR CSV Files
 
 Optional command-line arguments:
-    argv[1] = Name of first prepBUFR CSV file
-    argv[2] = Name of second prepBUFR CSV file
-    argv[3] = Name of combined prepBUFR CSV file
+    argv[1] = Text file containing names of input prepBUFR CSV files
+    argv[2] = Name of combined prepBUFR CSV file
 
 shawn.s.murdzek@noaa.gov
 Date Created: 25 July 2023
@@ -24,24 +23,28 @@ import pyDA_utils.bufr as bufr
 # Input Parameters
 #---------------------------------------------------------------------------------------------------
 
-bufr1_fname = ''
-bufr2_fname = ''
+bufr_list_fname = ''
 output_fname = ''
 
 # Option to use command-line arguments
 if len(sys.argv) > 1:
-    bufr1_fname = sys.argv[1]
-    bufr2_fname = sys.argv[2]
-    output_fname = sys.argv[3]
+    bufr_list_fname = sys.argv[1]
+    output_fname = sys.argv[2]
 
 
 #---------------------------------------------------------------------------------------------------
 # Combine CSV Files
 #---------------------------------------------------------------------------------------------------
 
-bufr1 = bufr.bufrCSV(bufr1_fname)
-bufr2 = bufr.bufrCSV(bufr2_fname)
-out_csv = bufr.combine_bufr(bufr1.df, bufr2.df)
+# Extract BUFR CSV file names to combine
+csv_in_list = []
+fptr = open(bufr_list_fname, 'r')
+for l in fptr:
+    csv_in_list.append(bufr.bufrCSV(l).df)
+fptr.close()
+
+# Combine BUFR CSV files
+out_csv = bufr.combine_bufr(csv_in_list)
 bufr.df_to_csv(out_csv, output_fname)
 
 
